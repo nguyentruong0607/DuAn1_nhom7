725e9b3fd7f2ecf67e5987b250d4dc7fe7c7c793
thaicris <thaintph28301@fpt.edu.vn>
2024-03-26 02:33:17 +0700

    abc

--- app/src/main/java/com/example/duan1_nhom7/activity/LoginActivity.java
+++ app/src/main/java/com/example/duan1_nhom7/activity/LoginActivity.java
@@ -17,10 +17,14 @@ import androidx.core.view.WindowInsetsCompat;
 import com.example.duan1_nhom7.DAO.UserDAO;
 import com.example.duan1_nhom7.MainActivity;
 import com.example.duan1_nhom7.R;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 TextView tv_loginRegister;
-EditText ed_user , ed_pass;
+    TextInputEditText ed_user , ed_pass;
+    private TextInputLayout text_inputLayoutLoginUser, text_inputLayoutLoginPass;
+
 Button btn_login;
     UserDAO userDAO;
 
@@ -51,18 +55,52 @@ Button btn_login;
         ed_user = findViewById(R.id.edt_loginUser);
         ed_pass = findViewById(R.id.edt_loginPass);
         btn_login =findViewById(R.id.btn_loginLogin);
+        text_inputLayoutLoginUser = findViewById(R.id.text_inputLayoutLoginUser);
+        text_inputLayoutLoginPass = findViewById(R.id.text_inputLayoutLoginPass);
     }
 
     private void Login(){
         String a = ed_user.getText().toString();
         String b = ed_pass.getText().toString();
-        if (userDAO.checkLogin(a,b)){
-            Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
+       if(valUser() | valPass()){
+           if (userDAO.checkLogin(a,b)){
+               Intent intent = new Intent(this, MainActivity.class);
+               startActivity(intent);
+           }
+           else {
+               Toast.makeText(this, "Sai tài khoản hoặc mật khẩu", Toast.LENGTH_SHORT).show();
+           }
+       }
+    }
+
+    private Boolean valUser() {
+        String valUser = text_inputLayoutLoginUser.getEditText().getText().toString().trim();
+
+        String check = "\\A\\w{4,20}\\z";
+        if (valUser.isEmpty()) {
+            text_inputLayoutLoginUser.setError("Không được bỏ trống");
+            return false;
+        } else if (!valUser.matches(check)) {
+            text_inputLayoutLoginUser.setError("Tên tài khoản không có khoản trắng");
+            return false;
         }
         else {
-            Toast.makeText(this, "sai tk mk", Toast.LENGTH_SHORT).show();
+            text_inputLayoutLoginUser.setError(null);
+            text_inputLayoutLoginUser.setErrorEnabled(false);
+            return true;
         }
 
+    }
+
+    private Boolean valPass() {
+        String valPass = text_inputLayoutLoginPass.getEditText().getText().toString().trim();
+        if (valPass.isEmpty()) {
+            text_inputLayoutLoginPass.setError("Không được bỏ trống");
+            return false;
+        } else {
+            text_inputLayoutLoginPass.setError(null);
+            text_inputLayoutLoginPass.setErrorEnabled(false);
+            return true;
+        }
     }
 }
